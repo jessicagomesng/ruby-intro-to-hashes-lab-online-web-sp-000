7f196d6109c8d6c7b6d1175bbe10b33c249ad14a
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-29 18:32:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
   monopoly = {railroads: {
     pieces: 4,
     names: {
-      reading_railrod: {},
+      reading_railroad: {},
       pennsylvania_railroad: {},
       b_and_o_railroad: {},
       shortline: {}},
